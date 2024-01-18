fdd94baf20baa54c1718d74de8fbd14d3b1e6c52
Siddhant Nagaria <siddhant.nagaria1234@gmail.com>
2024-01-18 13:40:26 +0530

    lc

--- 94_Binary_Tree_Inorder_Traversal.java
+++ 94_Binary_Tree_Inorder_Traversal.java
@@ -13,6 +13,11 @@
  *     }
  * }
  */
+
+
+ //using recursion - 
+
+
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
@@ -26,5 +31,27 @@ class Solution {
         helper(node.left, list);
         list.add(node.val);
         helper(node.right, list);
+    }
+}
+
+
+
+//using iteration -
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer>res = new ArrayList<>();
+        Stack<TreeNode>stack = new Stack<>();
+        TreeNode curr = root;
+        while(curr!=null||!stack.isEmpty()){
+            while(curr!=null){
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
     }
 }
