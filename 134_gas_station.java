5abe5f1e8b53432851e2687d5379f9363ef76391
Siddhant Nagaria <siddhant.nagaria1234@gmail.com>
2024-02-22 16:59:58 +0530

    java

--- 134_gas_station.java
+++ 134_gas_station.java
@@ -1,23 +1,43 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalGas=0;
-        int totalCost=0;
-        int tank=0;
-        int start=0;
+        int totalGas = 0;
+        int totalCost = 0;
+        int tank = 0;
+        int start = 0;
 
-        for(int i = 0; i<gas.length;i++){
-            totalGas+=gas[i];
-            totalCost+=cost[i];
-            tank+=gas[i]-cost[i];
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            tank += gas[i] - cost[i];
 
-            if(tank<0){
-                start= i + 1;
+            if (tank < 0) {
+                start = i + 1;
                 tank = 0;
             }
         }
-        if(totalGas<totalCost){
+        if (totalGas < totalCost) {
             return -1;
         }
         return start;
+    }
+}
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int pos = -1, curr = 0, total = 0;
+        for (int i = 0; i < gas.length; i++) {
+            int diff = gas[i] - cost[i];
+            curr += diff;
+            total += diff;
+
+            if (curr < 0) {
+                curr = 0;
+                pos = i;
+            }
+        }
+        if (total >= 0) {
+            return pos + 1;
+        }
+        return -1;
     }
 }
