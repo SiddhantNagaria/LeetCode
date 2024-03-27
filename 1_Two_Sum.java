99afac8933a1f9b606aa2a623c85ebcfebbdf3a8
Siddhant Nagaria <siddhant.nagaria1234@gmail.com>
2024-03-27 20:58:01 +0530

    twosum

---
+++ 1_Two_Sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer>indexMap = new HashMap<>();
+        for(int i =0; i<nums.length;i++){
+            Integer requiredNum = target - nums[i];
+            if(indexMap.containsKey(requiredNum)){
+                return new int[]{indexMap.get(requiredNum),i};
+            }
+            indexMap.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
+}
