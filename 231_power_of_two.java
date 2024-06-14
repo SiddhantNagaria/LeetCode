554777244be80bc802cb66b07f571ba35e4f394f
Siddhant Nagaria <siddhant.nagaria1234@gmail.com>
2024-06-14 22:05:44 +0530

    14-5-24

--- 231_power_of_two.java
+++ 231_power_of_two.java
@@ -9,3 +9,12 @@ class Solution {
         return n==1;
     }
 }
+
+//OR 
+
+
+    public boolean isPowerOfTwo(int n) {
+        if(n==0)return false;
+       long x = (long)n;
+        return (x & (x-1))==0;
+    }
