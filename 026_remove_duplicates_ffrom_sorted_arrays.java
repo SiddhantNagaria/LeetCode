081e34488f607d4a0ddefcf8e8af32f3ea8d6e34
Siddhant Nagaria <siddhant.nagaria1234@gmail.com>
2024-06-15 19:11:35 +0530

    15-6-24

--- 026_remove_duplicates_ffrom_sorted_arrays.java
+++ 026_remove_duplicates_ffrom_sorted_arrays.java
@@ -10,3 +10,16 @@ class Solution {
       return i+1;
   }
 }
+ //OR
+ 
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int j = 0; j<nums.length;j++){
+            if(nums[j]!=nums[i]){
+                nums[++i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
