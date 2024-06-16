1e31787f4149dbb8bce3fac5b5b3d0a49c92d962
Siddhant Nagaria <siddhant.nagaria1234@gmail.com>
2024-06-16 23:28:53 +0530

    16-6-24

--- 121_best_time_to_buy_and_sell_stocks.java
+++ 121_best_time_to_buy_and_sell_stocks.java
@@ -12,3 +12,18 @@ class Solution {
         return maxprofit;
     }
 }
+
+//OR
+
+class Solution {
+    public int maxProfit(int[] prices) {
+       int maxProfit = 0;
+       int minSoFar = prices[0];
+       for(int i = 0; i<prices.length;i++){
+        minSoFar = Math.min(minSoFar, prices[i]);
+        int profit = prices[i] - minSoFar;
+        maxProfit = Math.max(maxProfit, profit);
+       } 
+       return maxProfit;
+    }
+}
