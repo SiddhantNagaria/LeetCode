9e6c1fd0cfb0e72a1ac0c4d055ed9c14b7e993e2
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-06-26 22:07:44 +0530

    26-6-24

--- 015_Three_Sum.java
+++ 015_Three_Sum.java
@@ -1,33 +1,25 @@
+import java.util.*;
+
 class Solution {
-    public List<List<Integer>> threeSum(int[] arr) {
-        if (arr == null || arr.length < 3)
-            return new ArrayList<>();
-        // Sort the elements
-        Arrays.sort(arr);
-
-        Set<List<Integer>> result = new HashSet<>();
-
-        // Now fix the first element and find the other two elements
-        for (int i = 0; i < arr.length - 2; i++) {
-            
-            // Find other two elements using Two Sum approach
-            int left = i + 1;
-            int right = arr.length - 1;
-            while (left < right) {
-                int sum = arr[i] + arr[left] + arr[right];
-
-                if (sum == 0) {
-
-                    // Add the set, and move to find other triplets
-                    result.add(Arrays.asList(arr[i], arr[left], arr[right]));
-                    left++;
-                    right--;
-                } else if (sum < 0)
-                    left++;
-                else
-                    right--;
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0;i<nums.length;i++){
+           if(i>0 && nums[i]==nums[i-1]) continue;
+           int target = -nums[i];
+           int lo = i+1, hi = nums.length-1;
+           while(lo<hi){
+            if(nums[lo]+nums[hi]==target){
+                res.add(Arrays.asList(nums[lo],nums[hi],nums[i]));
+                while(lo<hi && nums[lo]==nums[lo+1]) lo++;
+                while(lo<hi && nums[hi]==nums[hi-1]) hi--;
+                lo++;
+                hi--;
             }
+            else if(nums[lo] + nums[hi]<target) lo++;
+            else hi--;
+           }
         }
-        return new ArrayList<>(result);
+        return res;
     }
 }
