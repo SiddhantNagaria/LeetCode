ec0b5b7ad5dd85e1037bbdf1451c7405fcbedd4d
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-07-13 01:37:30 +0530

    Leetcode 234

--- 234_palindrome_linked_list.java
+++ 234_palindrome_linked_list.java
@@ -25,3 +25,40 @@ class Solution {
         return left >= right;
     }
 }
+
+//OR
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+       //slow pointer move 1 step at a time
+       //fast pointer move 2 steps at a time
+       ListNode slow = head, fast = head;
+       while(fast!=null && fast.next!=null){
+        slow = slow.next;
+        fast = fast.next.next;
+       }
+       ListNode prev = null;
+       while(slow!=null){
+        ListNode tmp = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = tmp;
+       }
+       while(prev!=null){
+        if(head.val!=prev.val) return false;
+        head = head.next;
+        prev = prev.next;
+       }
+       return true;
+    }
+}
