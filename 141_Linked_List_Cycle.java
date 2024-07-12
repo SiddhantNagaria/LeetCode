e5b6dce2d08292b351e99f942e1f7c79ce114133
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-07-13 01:58:55 +0530

    comments added to Q-141

--- 141_Linked_List_Cycle.java
+++ 141_Linked_List_Cycle.java
@@ -11,6 +11,8 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        //slow pointer move 1 step at a time
+        //fast pointer move 2 steps at a time
         ListNode slow = head;
         ListNode fast = head;
         while(fast!=null && fast.next!=null){
