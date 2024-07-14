e72469192de5d17de142491aeafac05c522b5e48
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-07-15 00:17:50 +0530

    Q-704

--- 704_binary_search.java
+++ 704_binary_search.java
@@ -14,3 +14,22 @@ class Solution {
         return index;
     }
 }
+
+//
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int index=-1;
+        int start=0, end=nums.length-1, middle=(end+start)/2;
+        while(start<=end){
+            if (nums[middle]==target) return middle;
+            if (nums[middle]>target){
+                end=middle-1; middle=(end+start)/2;
+            }
+            if (nums[middle]<target){
+                start=middle+1; middle=(end+start)/2;
+            }
+        }
+        return index;
+    }
+}
