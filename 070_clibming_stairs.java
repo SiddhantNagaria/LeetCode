bfec3c26276dab04b9d8731f9c57b56972446862
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-05 21:28:52 +0530

    5-8-24, Q925

--- 070_clibming_stairs.java
+++ 070_clibming_stairs.java
@@ -9,16 +9,4 @@ class Solution {
         }
         return a;
     }
-=======
-class Solution {
-    public int climbStairs(int n) {
-        if(n<=3)return n;
-        int a = 3 , b =2;
-        for(int i = 0;i<n-3;i++){
-            a= a+b;
-            b= a-b;
-        }
-        return a;
-    }
->>>>>>> efe6d9558924fabdca066cfd1fc2de9d3afa928d
 }
