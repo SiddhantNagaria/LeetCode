abdd307ae98ceeba9a4c22c3a2793c5b124cddeb
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-18 15:51:47 +0530

    18-8-24, Q881 and some file renamed

--- 881_boats_to_save_people.java
+++ 881_boats_to_save_people.java
@@ -18,3 +18,26 @@ class Solution {
      return boats;
     }
  }
+
+//or
+
+ class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int boats = 0;
+        while(left<=right){
+            if(people[right]+ people[left]<=limit){
+                left++;
+                right--;
+                boats++;
+            }
+            else{
+                right--;
+                boats++;
+            }
+        }
+        return boats;
+    }
+}
