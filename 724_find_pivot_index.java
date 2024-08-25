d9a60a9a82517f41b1e4dc3e7e41b1fbc1b5e55b
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-26 00:12:52 +0530

    26-8-24, Q724

--- 724_find_pivot_index.java
+++ 724_find_pivot_index.java
@@ -15,3 +15,24 @@ class Solution {
         return -1;        
     }
 }
+
+//or
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        int left = 0;
+        int right = sum;
+        for (int i = 0; i < nums.length; i++) {
+            right -= nums[i];
+            if (right == left) {
+                return i;
+            }
+            left += nums[i];
+        }
+        return -1;
+    }
+}
