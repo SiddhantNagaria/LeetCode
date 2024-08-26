60c6644b382d88bf17e136df61db39b74ca06527
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-26 16:49:13 +0530

    26-8-24, Q763

--- 763_Partition_Labels.java
+++ 763_Partition_Labels.java
@@ -16,3 +16,24 @@ class Solution {
         return res;
     }
 }
+
+//or
+
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List <Integer> partitions = new ArrayList<>();
+        for(int i =0; i < s.length();){
+            int startIndex = i;
+            int endIndex = s.lastIndexOf(s.charAt(startIndex));
+            for(int k = startIndex+1; k<=endIndex -1; k++){
+                int lastIndexOfNextChar = s.lastIndexOf(s.charAt(k));
+                if(lastIndexOfNextChar>endIndex){
+                    endIndex = lastIndexOfNextChar;
+                }
+            }
+            partitions.add(endIndex - startIndex + 1);
+            i = endIndex + 1;
+        }
+        return partitions;
+    }
+}
