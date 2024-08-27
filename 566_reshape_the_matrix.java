00e6554075a10a3c4ee941a66ceeb740e33718dd
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-27 20:07:17 +0530

    27-8-24, Q566

--- 566_reshape_the_matrix.java
+++ 566_reshape_the_matrix.java
@@ -19,3 +19,29 @@ class Solution {
         return n_mat;
     }
 }
+
+
+//or
+
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        if (m * n != r * c) {
+            return mat;  // Return the original matrix if reshaping isn't possible
+        }
+        int[][] ans = new int[r][c];
+        int row = 0, col = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[row][col] = mat[i][j];
+                col++;
+                if (col == c) {
+                    row++;
+                    col = 0;
+                }
+            }
+        }
+        return ans;
+    }
+}
