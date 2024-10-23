0321e934e6ed86fc4a930438553e44b8a5084fbf
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-10-23 23:28:19 +0530

    23-10-24, Q75 - 3

--- 075_sort_colors.java
+++ 075_sort_colors.java
@@ -1,4 +1,6 @@
+
 class Solution {
+
     public void sortColors(int[] nums) {
         int start = 0;
         int mid = 0;
@@ -23,6 +25,7 @@ class Solution {
             }
         }
     }
+
     private void swap(int[] arr, int pos1, int pos2) {
         int temp = arr[pos1];
         arr[pos1] = arr[pos2];
@@ -30,20 +33,49 @@ class Solution {
     }
 }
 
-
 //////////////////////////////////////////////////////////////////////////////
 
 public class Solution {
+
     public void sortColors(int[] nums) {
-       for(int i = 0; i<nums.length;i++){
-        for(int j= i+1;j<nums.length;j++){
-            if(nums[i]>=nums[j]){
-                int temp = nums[i];
-                nums[i]=nums[j];
-                nums[j] = temp;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] >= nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
             }
         }
-       }
     }
 }
 
+////////////////////////////////////////////
+
+
+public class Solution {
+
+    public void sortColors(int[] a) {
+        int low = 0;
+        int mid = 0;
+        int high = a.length - 1;
+        while (mid <= high) {
+            if (a[mid] == 0) {
+                int temp = a[low];
+                a[low] = a[mid];
+                a[mid] = temp;
+                mid++;
+                low++;
+            } else if (a[mid] == 2) {
+                int temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+                high--;
+            } else {
+                mid++;
+            }
+        }
+    }
+}
+
+                           
