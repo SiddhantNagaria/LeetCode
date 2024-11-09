d830b04e8e9832f106d36aab25091101197ecf35
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-11-09 16:15:47 +0530

    Q-830

--- 830_position_of_large_groups.java
+++ 830_position_of_large_groups.java
@@ -20,3 +20,21 @@ class Solution {
         return result;
     }
 }
+
+///////////
+
+
+class Solution {
+    public List<List<Integer>> largeGroupPositions(final String s) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0, j = 0; i < s.length(); i = j) {
+            while(j<s.length() && s.charAt(i)==s.charAt(j)){
+                j++;
+            }
+            if(j-i>=3){
+                result.add(Arrays.asList(i,j-1));
+            }
+        }
+        return result;
+    }
+}
