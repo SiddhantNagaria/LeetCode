cdc9f78ff10b1454c1074f49a9c6b3aa62ed778a
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-11-21 23:50:51 +0530

    21-11-24 Q084

--- 084_MAximum_Area_in_Histogram.java
+++ 084_MAximum_Area_in_Histogram.java
@@ -1,19 +1,20 @@
+
 class Solution {
+
     public int largestRectangleArea(int[] arr) {
         int maxArea = 0;
         int nsr[] = new int[arr.length];
         int nsl[] = new int[arr.length];
-        
+
         //Next Smaller Right
         Stack<Integer> s = new Stack<>();
-        for(int i = arr.length - 1; i >= 0; i--){
-            while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
                 s.pop();
             }
-            if(s.isEmpty()){
+            if (s.isEmpty()) {
                 nsr[i] = arr.length;
-            }
-            else{
+            } else {
                 nsr[i] = s.peek();
             }
             s.push(i);
@@ -21,26 +22,52 @@ class Solution {
 
         //Next Smaller Left
         s = new Stack<>();
-        for(int i = 0; i <arr.length; i++){
-            while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
                 s.pop();
             }
-            if(s.isEmpty()){
+            if (s.isEmpty()) {
                 nsl[i] = -1;
-            }
-            else{
+            } else {
                 nsl[i] = s.peek();
             }
             s.push(i);
         }
 
         //Current Area : width = j-i-1 = nsr[i] - nsl[i] -1
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             int width = nsr[i] - nsl[i] - 1;
             int height = arr[i];
             int area = width * height;
             maxArea = Math.max(maxArea, area);
         }
         return maxArea;
+    }
+}
+
+////////////////
+
+
+class Solution {
+
+    public int largestRectangleArea(int[] histo) {
+        Stack<Integer> st = new Stack<>();
+        int maxA = 0;
+        int n = histo.length;
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() && (i == n || histo[st.peek()] >= histo[i])) {
+                int height = histo[st.peek()];
+                st.pop();
+                int width;
+                if (st.empty()) {
+                    width = i; 
+                }else {
+                    width = i - st.peek() - 1;
+                }
+                maxA = Math.max(maxA, width * height);
+            }
+            st.push(i);
+        }
+        return maxA;
     }
 }
